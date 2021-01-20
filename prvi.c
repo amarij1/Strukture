faafc1bd91375e5c622d64d81a10fca5a683fed9
amarij1 <amarij01@fesb.hr>
2021-01-20 18:14:31 +0100

    prvi

--- prvi.c
+++ prvi.c
@@ -2,64 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-typedef struct {
+struct student {
 	char ime[20];
 	char prez[20];
 	int bod;
-}student;
-
-int brojR();
-void unos(int, student*);
-void ispis(student*, int);
-
+};
+int brojR(char[]);
+int unos(int, struct student*, char[]);
+int ispis(int, struct student*, char[]);
 int main() {
-	int br;
-	//broj redaka
-	br = brojR();
-	student* s;
-	s = (student*)malloc(br * sizeof(student));
-	//unos u datoteku
-	unos(br, s);
-	//ispis datoteke
-	ispis(s, br);
+	int br = 0;
+	char dat[20];
+	struct student* st;
+	st = (struct student*)malloc(br * sizeof(struct student));
+	printf("Unesi ime datoteke\n");
+	scanf("%s", dat);
+	br = brojR(dat);
+	unos(br, st, dat);
+	ispis(br, st, dat);
+
 	return 0;
 }
 
-int brojR() {
+int brojR(char dat[]) {
 	FILE* ulz;
 	int br = 1;
-	ulz = fopen("proba.txt", "r");
+	char c;
+	ulz = fopen(dat, "r");
 	if (ulz == NULL) {
-		printf("Greska");
-		exit(1);
+		printf("Greska, dokument ne postoji");
 	}
-	while (!feof(ulz)) {
-		if (fgetc(ulz) == '\n') {
-			br++;
+	for (c = getc(ulz); c != EOF; c = getc(ulz)) {
+		if (c == '\n') {
+			br = br + 1;
 		}
 	}
 	fclose(ulz);
 	return br;
 }
 
-void unos(int br, student* s) {
+int unos(int br, struct student* st, char dat[]) {
+	int i = 0;
 	FILE* ulz;
-	int i;
-	ulz = fopen("proba.txt", "r");
+	ulz = fopen(dat, "r");
 	for (i = 0; i < br; i++) {
-		fscanf(ulz, "%s %s %d", s[i].ime, s[i].prez, &s[i].bod);
+		fscanf(ulz, "%s %s %d", (st + i)->ime, (st + i)->prez, &(st + i)->bod);
 	}
 	fclose(ulz);
 }
 
-void ispis(student* s, int br) {
+int ispis(int br, struct student* st, char dat[]) {
 	int maks = 50;
-	int i;
+	int i = 0;
 	for (i = 0; i < br; i++) {
-		printf("%s %s %d/%d %f\n", (s + i)->ime, (s + i)->prez, (s + i)->bod, maks, ((float)(s + i)->bod / (float)maks * 100));
+		printf("%s %s %d %d %d\n", (st + i)->ime, (st + i)->prez, (st + i)->bod, maks, ((st + i)->bod / maks * 100));
 	}
-
 }
-
